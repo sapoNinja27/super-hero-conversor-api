5fb84077791c4443b36c79bacd72b5019f760658
João Pedro Ronzani <joao.ronzani@betha.com.br>
2022-09-29 08:38:27 -0300

    First

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const routes = require('./routes')
 
-// app.use(cors())
+app.use(cors())
 app.use('/api', routes)
 app.listen(PORT, function(){
     console.log('Server initiated in port: ' + PORT)
